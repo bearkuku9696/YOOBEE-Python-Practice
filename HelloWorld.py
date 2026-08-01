41dc88aaceb1a198e1582db571196ec82db52db8
yu <bearkuku9696@gmail.com>
2026-08-02 09:20:51 +1200

    week1

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,20 @@
 
+def fib(num):
+    a = 0
+    b = 1
+    list_1=[a,b]
+    for i in range(num-2):
+        new=a+b
+        list_1.append(new)
+        a=b
+        b=new
+    print(list_1)
 
 print('hello world')
+
+var1 = int(input("Please enter an integer value:"))
+print(var1)
+
+fib(var1)
+
+
